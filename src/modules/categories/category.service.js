db6a13d438b2394bd698d66a3a76f573fae3903c
Full Stack Developer <104620467+full-stack-developer-pro@users.noreply.github.com>
2026-05-04 11:09:17 +0530

    categories

--- src/modules/categories/category.service.js
+++ src/modules/categories/category.service.js
@@ -16,7 +16,14 @@ exports.createCategory = async ({ name, description }) => {
 };
 
 exports.getCategories = async () => {
-  const categories = await prisma.category.findMany();
+  const categories = await prisma.category.findMany({
+    include: {
+      _count: {
+        select: { products: true },
+      },
+    },
+  });
+
   return categories;
 };
 
