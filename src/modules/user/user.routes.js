1b906f5f049268179a00ee252a568ed2f4371d8c
Full Stack Developer <104620467+full-stack-developer-pro@users.noreply.github.com>
2026-05-26 11:02:25 +0530

    c

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -4,9 +4,9 @@ const controller = require("./user.controller");
 
 router.post("/",authMiddleware, controller.createUser);
 router.get("/", authMiddleware, controller.getUsers);
+router.get("/admin-profile", authMiddleware, controller.adminProfile);
 router.get("/:id", authMiddleware, controller.getById);
 router.put("/:id", authMiddleware, controller.updateUser);
 router.delete("/:id", authMiddleware, controller.deleteUser);
-router.get("/admin-profile", authMiddleware, controller.adminProfile);
 
 module.exports = router;
